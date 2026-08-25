fdee13bda7f22104c0f739250225d6622e70eadf
anony <anonymoususer5761@gmail.com>
2026-08-25 11:49:01 +0530

    Added card to represent each book

--- script.js
+++ script.js
@@ -66,8 +66,10 @@ submitBookBtn.addEventListener("click", (event) => {
     form.synopsis.value,
     form.author.value,
     form.pages.value,
-    form.read.value,
+    form.read.checked,
   );
+
+  console.log(book.read);
 
   addBookToLibrary(book);
   addBookModal.close();
@@ -79,14 +81,56 @@ submitBookBtn.addEventListener("click", (event) => {
 
 const booksCardContianer = document.getElementById("books-card-container");
 
+const svg =
+  '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>';
+
 function displayLibrary() {
   booksCardContianer.innerHTML = "";
   for (let book of myLibrary) {
-    const card = document.createElement("div");
-    for (let data of book.info()) {
-      card.textContent += data;
-    }
+    let card = document.createElement("div");
     card.classList.add("books-card");
+
+    let title = document.createElement("p");
+    title.classList.add("title");
+    title.textContent = book.title;
+    card.appendChild(title);
+
+    let deleteBtnContainer = document.createElement("div");
+    deleteBtnContainer.classList.add("delete");
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = svg;
+    deleteBtnContainer.appendChild(deleteBtn);
+    card.appendChild(deleteBtnContainer);
+
+    let synopsis = document.createElement("div");
+    synopsis.classList.add("synopsis");
+    let synopsisHeader = document.createElement("p");
+    synopsisHeader.textContent = "Synopsis";
+    synopsis.appendChild(synopsisHeader);
+    let synopsisPara = document.createElement("p");
+    synopsisPara.textContent = book.synopsis;
+    synopsis.appendChild(synopsisPara);
+    card.appendChild(synopsis);
+
+    let read = document.createElement("div");
+    read.classList.add("read");
+    let readBtn = document.createElement("button");
+    readBtn.textContent = book.read ? "Mark Unfinished" : "Mark Finished";
+    readBtn.classList.add(book.read ? "finished" : "unfinished");
+    read.appendChild(readBtn);
+    card.appendChild(read);
+
+    let author = document.createElement("p");
+    author.classList.add("author");
+    author.textContent = book.author;
+    card.appendChild(author);
+
+    let pages = document.createElement("p");
+    pages.classList.add("pages");
+    pages.textContent = book.pages;
+    pages.setAttribute("title", "Number of Pages");
+    card.appendChild(pages);
+
     booksCardContianer.appendChild(card);
   }
 }
